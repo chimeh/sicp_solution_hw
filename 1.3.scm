f4487579d78372bbe98ed440edcef49bfe636ee5
jimagile <jimagile@gmail.com>
2013-06-29 22:55:12 +0800

    initial commit

---
+++ 1.3.scm
@@ -0,0 +1,1 @@
+﻿(define (sum_max2 a b c) (/ (* a b c) (min a b c)))
---
+++ 1.4.scm
@@ -0,0 +1,4 @@
+﻿(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+; This procedure receives two arguments, namely a and b, and
+; returns the sum of a and the absolute value of b
