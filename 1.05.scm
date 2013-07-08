529670501fdbe4b10ffe92a645c0b337bb676983
jimagile <jimagile@gmail.com>
2013-07-09 01:19:52 +0800

    finish 1.5-1.8

--- 1.05.scm
+++
@@ -1,5 +0,0 @@
-﻿; Note that ``p'' is recursively defined by itself. Therefore, for 
-; the applicative-order evaluation, ``p'' will be evaluated in the 
-; test, and this will lead to an infinite recursion. While in the 
-; normal-order evaluation, the value of p is not evaluated, and 
-; thus the test will return 0.
