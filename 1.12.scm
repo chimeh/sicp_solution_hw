de0903db593dbb7cf2da409b2fad416d9098029e
jimagile <jimagile@gmail.com>
2013-07-18 23:56:43 +0800

    merge develop to master

---
+++ 1.12.scm
@@ -0,0 +1,15 @@
+﻿
+(define (pascal-triangle row col) 
+  (cond ((> col row) 0) 
+        ((< col 0) 0) 
+        ((= col 1) 1) 
+        ((+ (pascal-triangle (- row 1) (- col 1)) 
+            (pascal-triangle (- row 1) col))))) 
+ 
+;; Testing 
+(pascal-triangle 1 1) 
+(pascal-triangle 2 2) 
+(pascal-triangle 3 2) 
+(pascal-triangle 4 2) 
+(pascal-triangle 5 2) 
+(pascal-triangle 5 3)
---
+++ sqrt.scm
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname sqrt) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+(define (good_enough x guess) (< (abs (- x
+                                         (* guess guess)))
+                                 0.000001))
+
+(define (improve guess x) (/  (+ guess (/ x guess)) 2 ))
+  
+(define (sqrt_iter x guess) (cond ((good_enough x guess) guess)
+                                  (else  (sqrt_iter x (improve guess x)))))
+  
+(define (sqrtt x) (sqrt_iter x 1))
+
+
+(sqrtt 10000)
+  
