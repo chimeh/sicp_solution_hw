e99d4ea71c2703170e7a70a62d8a693cb503178e
jimagile <jimagile@gmail.com>
2013-08-12 23:50:30 +0800

    add 1.29.scm

---
+++ 1.29.test.scm
@@ -0,0 +1,12 @@
+﻿ (define (simpson f a b n) 
+   (define fixed-n (round-to-next-even n)) 
+   (define h (/ (- b a) fixed-n)) 
+   (define (simpson-term k) 
+     (define y (f (+ a (* k h)))) 
+     (if (or (= k 0) (= k fixed-n)) 
+         (* 1 y) 
+         (if (even? k) 
+             (* 2 y) 
+             (* 4 y))))
+			 
+   (* (/ h 3) (sum simpson-term 0 inc fixed-n))) 
